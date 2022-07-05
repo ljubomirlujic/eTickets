9c1ad426946fd3b15a4650007610d8e2828c1107
ljubomirlujic <ljubomir.lujic60@gmail.com>
2022-07-05 17:15:03 +0200

    first commit

---
+++ eTickets/src/main/java/com/ftn/eTickets/ETicketsApplication.java
@@ -0,0 +1,13 @@
+package com.ftn.eTickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ETicketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ETicketsApplication.class, args);
+	}
+
+}
---
+++ eTickets/src/test/java/com/ftn/eTickets/ETicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftn.eTickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ETicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
